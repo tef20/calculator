421859c32c513dc6837b11158632a57e734b3183
Chris Telford <christelford@hotmail.co.uk>
2021-05-26 15:38:38 +0100

    Project requirements met
    Calculator features / functionality:
     - gui + keyboard interface
     - add, subtract, multiply, divide, backspace, clear all
     - add decimal
     - handles floating points by rounding to 2 d.p.
     - hover and click animations
    
    Future additions:
     - better documentation?
     - add button to flip sign (- / +)
     - user instructions key
     - bind keyboard press to gui button:active animations
     - calculator flip animation - press button, flip calc, show random calculator message

--- script.js
+++ script.js
@@ -1,141 +1,204 @@
-function equations () {
-    this.operands = {
-        num1: null,
-        num2: null
+// TODO: 
+// - instructions key
+// - calculator flip animation 'hello'
+
+FLIP_MESSAGE = "01134"
+WELCOME_MESSAGE = "...";
+ZERO_ERROR_MSG = "ZERO ERROR";
+
+class Calculator {
+    constructor(num1, num2, operator) {
+        this.operands = {
+            "num1": num1 ? num1 : null,
+            "num2": num2 ? num2 : null
+        };
+        this.operator = operator ? operator : null;
+        this.result = null;
     }
 
-    this.operator = null;
+    allClear = () => {
+        this.operands = {
+            "num1": null,
+            "num2": null
+        };
+        this.operator = null;
+        this.result = null;
+    }
     
-    this.getCurrentNum = () => (this.operator === null) ? 'num1' : 'num2';
-    
-    this.updateNum = (newNum) => {
+    handleNum = (newNum) => {       
         if (isNaN(newNum)) {
             console.error(`${newNum} is not a number.`);
-            return;
+        } else if (this.result !== null) {
+            this.allClear();
+            this.updateNum(newNum);
         } else {
-            const oldNum = this.getCurrentNum();
-            if (this.operands[oldNum] !== null) {
-                this.operands[oldNum] += newNum ;
-            } else {
-                this.operands[oldNum] = newNum;
-            }
+            this.updateNum(newNum);
         }
     }  
-
-    this.addDecimal = () => {
+    
+    updateNum = (newNum) => {
         const oldNum = this.getCurrentNum();
-        if (this.operands[oldNum] !== null && !(/\./.test(this.operands[oldNum]))) {
+        if (this.operands[oldNum] !== null) {
+            this.operands[oldNum] += newNum.toString();
+        } else {
+            this.operands[oldNum] = newNum.toString();
+        }
+    }
+
+    getCurrentNum = () => (this.operator === null) ? 'num1' : 'num2';
+
+    addDecimal = () => {
+        const oldNum = this.getCurrentNum();
+        if (this.result === null && this.operands[oldNum] !== null && !(/\./.test(this.operands[oldNum]))) {
             this.operands[oldNum] += '.';
         }
     }
 
-    this.updateOperator = (newOperator) => {
-        const currentOperand = this.getCurrentNum;
-        if (currentOperand === 'num1' && (/^[\+\-\*\/]$/.test(newOperator))) {
+    handleOperator = (newOperator) => {       
+        if (/^[\+\-\*\/]$/.test(newOperator)) {
+            if (this.result === ZERO_ERROR_MSG) {
+                return;
+            }
+            if (this.operands['num2'] !== null) {
+                this.operate();
+            }
+            if (this.result !== null) {
+                // feed prior result into new operation
+                let mem = this.result;
+                this.allClear();
+                this.updateNum(mem);
+            }
+            this.updateOperator(newOperator);
+        }
+    }
+    
+    updateOperator = (newOperator) => {
+        if (this.operands['num1'] !== null) {
             this.operator = newOperator;
         }
     }
 
-    this.delNum = () => {
-        // TODO
+    deleteOne = () => {
+        this.result = null;
+        let currentNum = this.getCurrentNum();
+        if (this.operands[currentNum] !== null) {
+            this.operands[currentNum] = this.operands[currentNum].slice(0, -1).length === 0 ? 
+                                            null : 
+                                            this.operands[currentNum].slice(0, -1);
+        } else {
+            this.updateOperator(null);
+        }
     }
 
-    // TODO 
+    operate = () => {
+        if (!Object.values(this.operands).includes(null)) {
+            switch(this.operator) {
+                case '+':
+                    this.result = this.add(this.operands['num1'], this.operands['num2']);
+                    return;
+                case '-':
+                    this.result = this.subtract(this.operands['num1'], this.operands['num2']);
+                    return;
+                case '*':
+                    this.result = this.multiply(this.operands['num1'], this.operands['num2']);
+                    return;
+                case '/':
+                    this.result = this.divide(this.operands['num1'], this.operands['num2']);
+                    return;
+                default:
+                    console.log(`Operator ${operator} has no match.`);
+                    return "ERROR";
+            }
+        }        
+    }
 
+    add = (num1, num2) => {
+        return Math.round((parseFloat(num1) + parseFloat(num2)) * 100) / 100;
+    }
+
+    subtract = (num1, num2) => {
+        return Math.round((parseFloat(num1) - parseFloat(num2)) * 100) / 100;
+    }
+
+    multiply = (num1, num2) => {
+        return Math.round((parseFloat(num1) * parseFloat(num2)) * 100) / 100;
+    }
+
+    divide = (num1, num2) => {
+        if (Math.round((parseFloat(num1) !== 0 && parseFloat(num2) !== 0) * 100) / 100) {
+            return Math.round((parseFloat(num1) / parseFloat(num2)) * 100) / 100;
+        } else {
+            return ZERO_ERROR_MSG;
+        }
+    }
 }
 
+// Initialize
+let calc = new Calculator();
+let display = document.getElementById('display');
+display.textContent = WELCOME_MESSAGE;
+console.log("Use onscreen buttons or keyboard values: \
+            \n- numbers 0-9\
+            \n- operations + - / and *\
+            \n- press enter or = to perform calculations\
+            \n- press backspace to undo an action\
+            \n- press c for All Clear")
 
 
 // Bindings
-window.addEventListener('click', handleInput)
+window.addEventListener('click', handleMouseClick);
+window.addEventListener('keydown', handleKeyDown)
 
-function handleInput (e) {
-    // if not button - return
-    if (!e.target.classList.contains('button')) {
-        console.log("not button!");
-        return;
-    } else {
-        console.log(e.target.textContent)
-        switch(true) {
-            case !isNaN(e.target.textContent):
-                // if ()          
-        //      if first number (operator called? no)
-        //          if num1 exists
-        //              append to num1
-        //          else
-        //              initialize num1
-        //      if second number (operator called? yes)
-        //          if num2 exists
-        //              append to num2
-        //          else
-        //              initialize num2
-        //      else (edge cases?)
-        //          return
-        // if point
-        //      
-        // else if operator
-        //      if num1 exists && num2 doesn't exist
-        //          update operator
-        //      else (edge cases?)
-        //          return
-        // else if AC
-        //      reset all
-        // else if result
-        //     if num1 && num2 && operator exist
-        //          run operate(operator, num1, num2);
-        //     else
-        //          return
-        // ??else if percentage
-        //     if num2 exist
-        //          update num2 to num2 / 100
-        //     if operator exists
-        //          return;
-        //     if num1 exist
-        //          update num1 to num1 / 100
-        //     
+function handleMouseClick (e) {
+    if (e.target.classList.contains('button')) {
+        operateButton(e.target.textContent);
+    }
+}
+
+function handleKeyDown (e) {
+    if (/^[0-9\+\-\*\/\.=c]$|Backspace|Enter/.test(e.key)) {
+        operateButton(e.key);
+    }
+}
+
+function operateButton(buttonName) {
+    switch(true) {
+        case /^[0-9]$/.test(buttonName):
+            calc.handleNum(buttonName);
+            break;
+        case /^[\+\-\*\/]$/.test(buttonName): 
+            calc.handleOperator(buttonName);
+            break;
+        case /^\.$/.test(buttonName): 
+            calc.addDecimal();
+            break;
+        case /^DEL$|^Backspace$/.test(buttonName): 
+            calc.deleteOne();
+            break;         
+        case /^AC$|^[c]$/.test(buttonName): 
+            calc.allClear();
+            break;
+        case /^=$|^Enter$/.test(buttonName): 
+            calc.operate();
+            break;                
         default:
-                console.error(`no case match.`);
-                return "ERROR";
-            }
+            console.error(`Button has no match.`);
+            break;
+    }
+    updateDisplay(calc);
+}
+
+function updateDisplay (calculatorObject) {
+    if (calc.result !== null) {
+        display.textContent = calc.result;
+    } else if (calc.operands["num2"] !== null) {
+        display.textContent = `${calc.operands["num1"]} ${calc.operator} ${calc.operands["num2"]}`;
+    } else if (calc.operator) {
+        display.textContent = `${calc.operands["num1"]} ${calc.operator} `;
+    } else if (calc.operands["num1"]) {
+        display.textContent = calc.operands["num1"];
+    } else {
+        display.textContent = '...';
     }
     
-    //      
-    if (e.target.classList.contains('button number')) {
-        console.log("number!")
-    } else {
-        console.log("not a number!")
-    }
-}
-
-// Calculator Logic //
-function operate (operator, num1, num2) {
-    switch(operator) {
-        case '+':
-            return add(num1, num2);
-        case '-':
-            return subtract(num1, num2);
-        case '*':
-            return multiply(num1, num2);
-        case '/':
-            return divide(num1, num2);
-        default:
-            console.error(`Operator ${operator} has no match.`);
-            return "ERROR";
-        }
-}
-
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 !== 0 && num2 !== 0 ? num2 / num2 : "ZERO ERROR";
 }
